58944192e2b59eb78d9a85ae87a37617b7fda017
Ville M. Vainio <ville.ma.vainio@nokia.com>
2012-09-21 22:35:22 +0300

    format source

--- src/com/w/JPodcast.java
+++ src/com/w/JPodcast.java
@@ -19,109 +19,108 @@ import javax.microedition.midlet.*;
  * @author v6vainio
  */
 public class JPodcast extends TantalumMIDlet implements CommandListener {
-	private DisplayManager manager;
-	private Displayable screen1;
-	private Displayable screen2;
-	private Displayable screen3;
-	private Command	 back;
-	private Command	 next;
-	private Command cmdSel;
-	
-	/**
-	 * Creates several screens and navigates between them.
-	 */
-	public JPodcast() {
-            super(4);
-            this.manager = new DisplayManager(Display.getDisplay(this));
-            this.back = new Command("Back", Command.BACK, 1);
-            this.next = new Command("Next", Command.OK, 1);
-            cmdSel =    new Command("Select", Command.ITEM, 1 );
 
-            this.screen1 = getSreen1();
-            //this.screen1.setCommandListener(this);
-            this.screen1.addCommand(this.back);
-            this.screen1.addCommand(this.next);
+    private DisplayManager manager;
+    private Displayable screen1;
+    private Displayable screen2;
+    private Displayable screen3;
+    private Command back;
+    private Command next;
+    private Command cmdSel;
 
-            this.screen2 = getSreen2();
-            this.screen2.setCommandListener(this);
-            this.screen2.addCommand(this.back);
-            this.screen2.addCommand(this.next);
+    /**
+     * Creates several screens and navigates between them.
+     */
+    public JPodcast() {
+        super(4);
+        this.manager = new DisplayManager(Display.getDisplay(this));
+        this.back = new Command("Back", Command.BACK, 1);
+        this.next = new Command("Next", Command.OK, 1);
+        cmdSel = new Command("Select", Command.ITEM, 1);
 
-            this.screen3 = getSreen3();
-            this.screen3.setCommandListener(this);
-            this.screen3.addCommand(this.back);
-            this.screen3.addCommand(this.next);
-	}
+        this.screen1 = getSreen1();
+        //this.screen1.setCommandListener(this);
+        this.screen1.addCommand(this.back);
+        this.screen1.addCommand(this.next);
 
-        
-	private void startFetch(String url) {
-            L.i("", "Starting to fetch");
-            
-		
-	}
-	private Displayable getSreen1() {
-            List l = new List("List [Screen 1]", List.IMPLICIT);
-            l.append("Engadget podcast", null);
-            l.append("The verge podcast", null);
-            final JPodcast app = this;
-            l.setSelectCommand( cmdSel );
-            l.setCommandListener(new CommandListener() {			
-                    public void commandAction(Command arg0, Displayable arg1) {				
-                            L.i("", "Podcast select!");
-                            // TODO Auto-generated method stub
-                            app.manager.next(app.screen2);
-                            startFetch("http://www.engadget.com/tag/podcasts/rss.xml");
+        this.screen2 = getSreen2();
+        this.screen2.setCommandListener(this);
+        this.screen2.addCommand(this.back);
+        this.screen2.addCommand(this.next);
+
+        this.screen3 = getSreen3();
+        this.screen3.setCommandListener(this);
+        this.screen3.addCommand(this.back);
+        this.screen3.addCommand(this.next);
+    }
+
+    private void startFetch(String url) {
+        L.i("", "Starting to fetch");
 
 
-                    }
-            });
-            return l;
-		
-	}
+    }
 
-	private Displayable getSreen2() {
-            return new List("List [Screen 2]", List.IMPLICIT);
-	}
+    private Displayable getSreen1() {
+        List l = new List("List [Screen 1]", List.IMPLICIT);
+        l.append("Engadget podcast", null);
+        l.append("The verge podcast", null);
+        final JPodcast app = this;
+        l.setSelectCommand(cmdSel);
+        l.setCommandListener(new CommandListener() {
+            public void commandAction(Command arg0, Displayable arg1) {
+                L.i("", "Podcast select!");
+                // TODO Auto-generated method stub
+                app.manager.next(app.screen2);
+                startFetch("http://www.engadget.com/tag/podcasts/rss.xml");
 
-	private Displayable getSreen3() {
-            return new TextBox("Text [Screen 3]", "", 100, TextField.ANY);
-	}
-	
-	/* (non-Javadoc)
-	 * @see javax.microedition.midlet.MIDlet#startApp()
-	 */
-	protected void startApp() throws MIDletStateChangeException {
-            this.manager.next(this.screen1);
-	}
-	
-	/* (non-Javadoc)
-	 * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
-	 */
-	public void commandAction(Command command, Displayable displayable) {
-            System.out.println("Command");
-            if (command == this.next) {
-                    if (displayable == this.screen1) {
-                            this.manager.next(this.screen2);
-                    } else
-                    if (displayable == this.screen2) {
-                            this.manager.next(this.screen3);
-                    }
+
             }
+        });
+        return l;
 
-            if (command == this.back) {
-                    this.manager.back();
+    }
+
+    private Displayable getSreen2() {
+        return new List("List [Screen 2]", List.IMPLICIT);
+    }
+
+    private Displayable getSreen3() {
+        return new TextBox("Text [Screen 3]", "", 100, TextField.ANY);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.microedition.midlet.MIDlet#startApp()
+     */
+    protected void startApp() throws MIDletStateChangeException {
+        this.manager.next(this.screen1);
+    }
+
+    /* (non-Javadoc)
+     * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
+     */
+    public void commandAction(Command command, Displayable displayable) {
+        System.out.println("Command");
+        if (command == this.next) {
+            if (displayable == this.screen1) {
+                this.manager.next(this.screen2);
+            } else if (displayable == this.screen2) {
+                this.manager.next(this.screen3);
             }
-	}
-	
-	/* (non-Javadoc)
-	 * @see javax.microedition.midlet.MIDlet#destroyApp(boolean)
-	 */
-	//protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {}
+        }
 
-	/* (non-Javadoc)
-	 * @see javax.microedition.midlet.MIDlet#pauseApp()
-	 */
-	protected void pauseApp() {}
+        if (command == this.back) {
+            this.manager.back();
+        }
+    }
 
+    /* (non-Javadoc)
+     * @see javax.microedition.midlet.MIDlet#destroyApp(boolean)
+     */
+    //protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {}
 
+    /* (non-Javadoc)
+     * @see javax.microedition.midlet.MIDlet#pauseApp()
+     */
+    protected void pauseApp() {
+    }
 }
